db76d8f6ebf4b9742c81d04d19a4fb9211520f97
Jason Salazar <jrsuperstar81@yahoo.com>
2021-05-11 10:19:20 -0600

    Updated MongoDB connection algorithm (as per Grimes' suggestion)

--- server/models/db.js
+++ server/models/db.js
@@ -9,7 +9,15 @@ const url =
     `@${mongoServer}/${mongoDBName}?retryWrites=true&w=majority`;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB Atlas...'))
-    .catch(err => console.error('Could not connect to MongoDB Atlas...', err))
+
+// .then(() => console.log('Connected to MongoDB Atlas...'))
+// .catch(err => console.error('Could not connect to MongoDB Atlas...', err))
+
+// Taking Tony Grimes' advice and using Greg's way of coding this MongoDB connection algorithm
+const db = mongoose.connection;
+db.once('open', (_) =>
+    console.log('MongoDB is now connected:', `${mongoUser}@${mongoServer}/${mongoDBName}`)
+);
+db.on('error', (err) => console.error('MongoDB connection error!', err));
 
 module.exports = mongoose
