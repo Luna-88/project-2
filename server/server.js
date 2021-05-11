8c04b482293af694614396f1594f163946f3852d
AndyH <andyb6@gmail.com>
2021-05-11 09:25:57 -0600

    db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const db = require('./database/connection')
+const db = require('./models/db')
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log('Server listening on port ' + port)
+    console.log(`Server listening on port: ${port}`)
 })
