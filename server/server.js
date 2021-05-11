726051aca9c3bc398556c8fa0e2124d8666a1f85
Jason Salazar <jrsuperstar81@yahoo.com>
2021-05-11 15:07:29 -0600

    Update Server.js file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const db = require('./models/db')
 
+
+// This middleware will look for form submission headers and add the data to req.body.
+// express.urlencoded() is used if the data was submitted using a traditional form.
+// express.json() is used if data was submitted as json using fetch() or similar.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
