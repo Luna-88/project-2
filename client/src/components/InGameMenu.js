a67c2d966e36e7fe6a54c7663c5e12914a126237
araadt <andy@vanderraadt.codes>
2021-05-24 21:52:35 -0600

    fix in-game menu / reorg css

--- client/src/components/InGameMenu.js
+++ client/src/components/InGameMenu.js
@@ -8,9 +8,9 @@ export default function InGameMenu() {
 
     function hasItem(item) {
         if (item === false || item.length === 0) {
-            return "❌"
+            return '❌'
         } else if (item === true) {
-            return "🔫"
+            return '🔫'
         } else {
             return item
         }
@@ -21,12 +21,15 @@ export default function InGameMenu() {
             let requestOptions = {
                 method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json'
-                }
+                    'Content-Type': 'application/json',
+                },
             }
 
             try {
-                let serverResponse = await fetch('/api/in-game-menu/inventory', requestOptions)
+                let serverResponse = await fetch(
+                    '/api/in-game-menu/inventory',
+                    requestOptions
+                )
                 if (serverResponse.status !== 200) {
                     let errorMessage = await serverResponse.text()
                     console.log('We had an error: ', errorMessage)
@@ -34,55 +37,89 @@ export default function InGameMenu() {
                 } else if (serverResponse.status === 200) {
                     let inventory = await serverResponse.json()
                     setInventoryItem(inventory)
-                }
-                else {
+                } else {
                     setServerResponse(undefined)
                 }
-            }
-            catch (error) {
-                console.error("Failed to reach the server")
+            } catch (error) {
+                console.error('Failed to reach the server')
             }
         }
         getInventory()
     }, [])
 
     return (
-        <div className='in-menu-container'>
+        <div className="in-menu-container">
             <div class="dropdown">
-                <button>Inventory</button>
-                {inventoryItem && <div class="dropdown-content">
-                    <div>Gaia Gun: {hasItem(inventoryItem.gaiaGun)}</div>
-                    <div>Cartridges: {hasItem(inventoryItem.cartridge)}</div>
-                    <div>Spaceship Pieces: {hasItem(inventoryItem.spaceshipPieces)}</div>
-                    <div>Hard Drive Pieces: {hasItem(inventoryItem.hardDrivePieces)}</div>
-                </div>}
+                <button class="menu-button">Inventory</button>
+                {inventoryItem && (
+                    <div class="dropdown-content">
+                        <div>Gaia Gun: {hasItem(inventoryItem.gaiaGun)}</div>
+                        <div>
+                            Cartridges: {hasItem(inventoryItem.cartridge)}
+                        </div>
+                        <div>
+                            Spaceship Pieces:{' '}
+                            {hasItem(inventoryItem.spaceshipPieces)}
+                        </div>
+                        <div>
+                            Hard Drive Pieces:{' '}
+                            {hasItem(inventoryItem.hardDrivePieces)}
+                        </div>
+                    </div>
+                )}
             </div>
 
             <button
-                onClick={() => handleClickWithFetch(
-                    setServerResponse,
-                    'GET',
-                    '/api/in-game-menu/tooltips')}
-            >Tooltips</button>
+                class="menu-button"
+                onClick={() =>
+                    handleClickWithFetch(
+                        setServerResponse,
+                        'GET',
+                        '/api/in-game-menu/tooltips'
+                    )
+                }
+            >
+                Tooltips
+            </button>
             <button
-                onClick={() => handleClickWithFetch(
-                    setServerResponse,
-                    'GET',
-                    '/api/in-game-menu/options')}
-            >Options</button>
+                class="menu-button"
+                onClick={() =>
+                    handleClickWithFetch(
+                        setServerResponse,
+                        'GET',
+                        '/api/in-game-menu/options'
+                    )
+                }
+            >
+                Options
+            </button>
             <button
-                onClick={() => handleClickWithFetch(
-                    setServerResponse,
-                    'GET',
-                    '/api/in-game-menu/save-game')}
-            >Save Game</button>
+                class="menu-button"
+                onClick={() =>
+                    handleClickWithFetch(
+                        setServerResponse,
+                        'GET',
+                        '/api/in-game-menu/save-game'
+                    )
+                }
+            >
+                Save Game
+            </button>
             <button
-                onClick={() => handleClickWithFetch(
-                    setServerResponse,
-                    'GET',
-                    '/api/in-game-menu/exit-game')}
-            >Exit Game</button>
-            {serverResponse && <div>{serverResponse}</div>}
+                class="menu-button"
+                onClick={() =>
+                    handleClickWithFetch(
+                        setServerResponse,
+                        'GET',
+                        '/api/in-game-menu/exit-game'
+                    )
+                }
+            >
+                Exit Game
+            </button>
+            <div class="server-response">
+                {serverResponse && <div>{serverResponse}</div>}
+            </div>
         </div>
     )
 }
