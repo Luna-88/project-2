7371955fd9f0615f8f935b3f9ca76b783060e652
AndyH <andyb6@gmail.com>
2021-06-01 19:29:43 -0600

    loads game

--- server/routes/playerMenu.js
+++ server/routes/playerMenu.js
@@ -3,19 +3,8 @@ const playerMenuRouter = express.Router()
 const Games = require('../models/game')
 
 const { verifyTokenFromCookies, getCookies } = require('../models/cookies')
-const { createGame } = require('../models/playerMenu')
+const { createGame, deleteGame } = require('../models/playerMenu')
 const { loadGame } = require('../middleware/loadGame')
-const { deleteGame } = require('../models/playerMenu')
-
-playerMenuRouter.post('/add-player', async (request, response) => {
-    try {
-        response.status(200).json({ message: 'Player was added' })
-    }
-    catch (error) {
-        console.log(error)
-        response.status(500).send('There was a problem adding player')
-    }
-})
 
 playerMenuRouter.post('/new-game', async (request, response) => {
     try {
@@ -27,28 +16,9 @@ playerMenuRouter.post('/new-game', async (request, response) => {
     }
 })
 
-//ADD DATA TO COOKIES
-playerMenuRouter.post('/load-game', async (request, response) => {
+playerMenuRouter.post('/load-game', loadGame, async (request, response) => {
     try {
-        let userId = verifyTokenFromCookies(request, 'accessToken', 'userId')
-        let gameId = request.body.gameId
-console.log("gameId ", gameId)
-        let loadGameCookie = getCookies(request)['loadedGame']
-
-        if (loadGameCookie === undefined) {
-            let loadGame = []
-
-            await Games.find({ _id: gameId, userId: userId })
-                .sort({ _id: -1 })
-                // .select("-_id")
-                // .select("-__v")
-                .then((game) => {
-
-                    response.send(game)
-                }
-                )
-            // response.status(200).send('Game loaded')
-        }
+        response.status(200).send('Game loaded')
     }
     catch (error) {
         console.log(error)
